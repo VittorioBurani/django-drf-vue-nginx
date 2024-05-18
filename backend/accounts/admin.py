3b92b08ba07bdb650a0cf172ed2e1f49223e6544
vitbur <vittorioburani@gmail.com>
2024-05-18 18:49:36 +0200

    Improve backend CustomUser management

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,10 +1,29 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from accounts.models import CustomUser
+from accounts.forms import CustomUserCreationForm, CustomUserChangeForm
 
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    [field.name for field in CustomUser._meta.get_fields()]
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    list_display = ("username", "email", "is_staff", "is_active",)
+    list_filter = ("username", "email", "is_staff", "is_active",)
+    fieldsets = (
+        (None, {"fields": ("username", "email", "password")}),
+        ("Permissions", {"fields": ("is_superuser", "is_staff", "is_active", "groups", "user_permissions")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "username", "email", "password1", "password2", "is_superuser",
+                 "is_staff", "is_active", "groups", "user_permissions"
+            )}
+        ),
+    )
+    search_fields = ("email",)
+    ordering = ("email",)
 
 admin.site.register(CustomUser, CustomUserAdmin)
