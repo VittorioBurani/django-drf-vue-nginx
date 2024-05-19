f05593ae8fa0ad6404e118c8c7b1bd11735b7036
vitbur <vittorioburani@gmail.com>
2024-05-19 02:54:09 +0200

    Map all basic backed endpoints in frontend

--- frontend/src/helpers/common-urls.js
+++ frontend/src/helpers/common-urls.js
@@ -1,9 +1,32 @@
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
-const BACKEND_API_URL = `${BACKEND_URL}/api`
-const BACKEND_API_AUTH_URL = `${BACKEND_API_URL}/auth`
+const BACKEND_API_URL = `${BACKEND_URL}/api`;
+const BACKEND_API_AUTH_URL = `${BACKEND_API_URL}/auth`;
 
-export default {
-    BACKEND_API_URL,
-    BACKEND_API_AUTH_URL,
-}
+// User endpoints:
+const userDataUrl = `${BACKEND_API_AUTH_URL}/get-user/`;
+const passwordResetUrl = `${BACKEND_API_AUTH_URL}/password-reset/`;
+
+// JWT endpoints:
+const accessTokenUrl = `${BACKEND_API_AUTH_URL}/token/`;
+const refreshTokenUrl = `${BACKEND_API_AUTH_URL}/token/refresh/`;
+const verifyTokenUrl = `${BACKEND_API_AUTH_URL}/token/verify/`;
+
+// Knox endpoints:
+const loginUrl = `${BACKEND_API_AUTH_URL}/login/`;
+const logoutUrl = `${BACKEND_API_AUTH_URL}/logout/`;
+const logoutAllUrl = `${BACKEND_API_AUTH_URL}/logoutall/`;
+
+export {
+    // User calls:
+    userDataUrl,
+    passwordResetUrl,
+    // JWT Calls:
+    accessTokenUrl,
+    refreshTokenUrl,
+    verifyTokenUrl,
+    // Knox calls:
+    loginUrl,
+    logoutUrl,
+    logoutAllUrl,
+};
