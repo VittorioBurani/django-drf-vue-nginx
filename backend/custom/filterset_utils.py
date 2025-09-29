be90ad471a3c2e7e89be97e2790b460ae2203a46
vitbur <vittorioburani@gmail.com>
2025-09-30 01:36:32 +0200

    Move scripts to `custom` root + add `CommonModelFilter` filterset

--- backend/custom/filterset_utils.py
+++ backend/custom/filterset_utils.py
@@ -1,5 +1,6 @@
 from typing import List
 from django.db import models
+from django_filters import rest_framework as filters
 
 
 MODEL_FIELDS_FILTER_MAP = {
@@ -29,6 +30,13 @@ MODEL_FIELDS_FILTER_MAP = {
     models.TimeField:     ['exact', 'in', 'gt', 'gte', 'lt', 'lte'],
     # Boolean Field:
     models.BooleanField: ['exact'],
+    # JSON, Array, Dict Field:
+    models.JSONField: [],
+    # File Field:
+    # set to empy list just to temporarily neutralize the issue:
+    # https://github.com/carltongibson/django-filter/discussions/1506
+    # https://github.com/carltongibson/django-filter/discussions/1478
+    models.FileField: [],
 }
 
 
@@ -40,3 +48,25 @@ def get_model_field_filter(model_field:models.Field) -> list:
 def get_model_fields_filters(model_class:models.Model.__class__, prefix:str='', exclude:List[str]=[]) -> dict:
     '''Get filter for specified models.Model subclass fields'''
     return {f'{prefix}{field.name}': get_model_field_filter(field) for field in model_class._meta.get_fields() if field.name not in exclude}
+
+
+class CommonModelFilter(filters.FilterSet):
+    # This filter accepts a number but doesn't filter on a model field directly.
+    # Instead, it calls the method 'filter_by_limit'.
+    limit = filters.NumberFilter(method='filter_by_limit', label='Limit number of results')
+
+    def filter_by_limit(self, queryset, name, value):
+        """
+        Orders the queryset by the 'id' field in descending order to get the
+        latest objects, and then limits the result set to the number
+        specified in the 'limit' query parameter.
+        """
+        # Ensure value is an integer before slicing
+        try:
+            limit = int(value)
+        except (ValueError, TypeError):
+            # If the value is invalid, just return the original queryset
+            return queryset
+        # `order_by('-id')` is a reliable way to get the "latest" objects.
+        # You could also use a timestamp field like '-created_at'.
+        return queryset.order_by('-id')[:limit]
